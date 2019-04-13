1ee980ce00cea16a90bc07ac5242ea878bb69d35
João Lucas Fernandes de Melo <lucas.fernandes@icomp.ufam.edu.br>
2019-04-13 01:19:31 -0400

    Questão 2

--- Trab2.c
+++ Trab2.c
@@ -1,1 +1,54 @@
-aaa
+#include "stdio.h"
+#include "Vetor.h"
+#include "Lista.h"
+#include <time.h>
+#include <stdlib.h>
+
+int main(){
+	ListaEncadeada lista;
+ 	int *vetor1,chave[30];
+ 	int tam = 1000000;
+	vetor1 = (int *)malloc(1000000*sizeof(int));
+ 	clock_t tempo1,tempo2;
+ 	double media =0;
+ 	double tempo[30];
+ 	int vseq[30];
+ 	PreencheVetorOrdenado(tam, vetor1);
+ 	CriaListaEncadeada(&lista);
+ 	CriaListaEVetorIguais(tam,&lista, vetor1);
+	 for(int i =0;i<30;i++){
+	 	chave[i] =(rand()%100+1);
+	 }
+
+	for(int i =0;i<30;i++){
+		tempo1 = clock();
+		BuscaSequencialVetor(chave[i],tam,vetor1);
+		tempo2 = clock();
+		tempo[i] = ((double)(tempo2-tempo1)/(CLOCKS_PER_SEC/1000));
+	 }
+
+	 printf("BUSCA SEQUENCIAL VETOR \n");
+	 for(int i =0;i<30;i++){
+	 	printf("%lf\n",tempo[i]);
+	 }
+	 for(int i = 0;i<30;i++){
+	 	media +=tempo[i];
+	 }
+	 printf("\n%lf\n",(media/30));
+	 for(int i=0;i<30;i++){
+	 	tempo1 = clock();
+	 	BuscaSequencialListaEncadeada(chave[i], lista);
+	 	tempo2 = clock();
+	 	tempo[i] = ((double)(tempo2-tempo1)/(CLOCKS_PER_SEC/1000));
+	 }
+	 printf("BUSCA SEQUENCIAL LISTA \n");
+	 for(int i =0;i<30;i++){
+	 	printf("%lf\n",tempo[i]);
+	 }
+	 for(int i = 0;i<30;i++){
+	 	media +=tempo[i];
+	 }
+	 printf("\n%lf\n",(media/30));
+	 free(vetor1);
+	 DestruirLista(&lista);
+	}
